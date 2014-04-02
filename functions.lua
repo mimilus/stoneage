80ce4b7e1e923f209a8f0ff7571d5a77dc197e85
mimilus <mimilus@users.noreply.github.com>
2014-04-02 23:11:14 +0200

    Revert

--- functions.lua
+++ functions.lua
@@ -20,3 +20,51 @@ function stoneage.strike_fire(user, pointed_thing)
 				return
 			end
 end
+
+-- torchdecay
+-- burntime equals half a night
+if torchdecay then
+minetest.register_abm({
+	nodenames = {"default:torch"},
+	interval = 9,
+	chance = 1,
+	action = function(pos, node)
+		local meta = minetest.env:get_meta(pos)
+		local decay = meta:get_int("decay")
+		if not decay then
+			meta:set_int("decay", 1)
+			return
+		end
+		if decay >= math.random(36, 44) then
+			node.name = "stoneage:torch_unlit"
+			minetest.env:add_node(pos, node)
+			meta:set_int("decay", 0)
+			return
+		end
+		decay = decay + 1
+		meta:set_int("decay", decay)
+	end
+})
+
+minetest.register_abm({
+	nodenames = {"stoneage:bonfire"},
+	interval = 18,
+	chance = 1,
+	action = function(pos, node)
+		local meta = minetest.env:get_meta(pos)
+		local decay = meta:get_int("decay")
+		if not decay then
+			meta:set_int("decay", 1)
+			return
+	end
+		if decay >= math.random(36, 44) then
+			node.name = "stoneage:bonfire_unlit"
+			minetest.env:add_node(pos, node)
+			meta:set_int("decay", 0)
+			return
+		end
+		decay = decay + 1
+		meta:set_int("decay", decay)
+	end
+})
+end
